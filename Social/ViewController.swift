7283925d3277e9a87c3d14fee327ffa9cc6655a3
Samantha Wiley <samantha.wiley@student.csulb.edu>
2018-11-26 17:03:25 -0800

    Initial Commit

---
+++ Social/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Social
+//
+//  Created by Samantha Wiley on 11/26/18.
+//  Copyright © 2018 Samantha Wiley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
